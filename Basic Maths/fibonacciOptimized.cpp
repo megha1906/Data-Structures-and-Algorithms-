d927d01d9a60c7b84ae942ec24bcbf7b42ab932f
mbansal <mbansal@vmware.com>
2022-09-28 01:31:57 +0530

    Basic maths coding

---
+++ Basic Maths/fibonacciOptimized.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if( n == 0 ){
+        cout << 0 << endl;
+    } else {
+        int secondlast = 0;
+        int last = 1;
+        cout << secondlast << " " << last << " ";
+        int current;
+        for(int i = 2; i <= n; i++)
+        {
+            current = secondlast + last;
+            secondlast = last;
+            last = current;
+            cout << current << " ";
+        }
+        cout << endl;
+    }
+}
